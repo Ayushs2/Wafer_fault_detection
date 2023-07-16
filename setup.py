f4fd199273ccc4b3ec2dfb19a6284e9e3191ae6e
Ayuhshs2 <singh.ayush0105@gmail.com>
2023-07-16 10:59:38 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+name="src",
+version="0.0.1",
+author="Ayush",
+author_email="singh.ayush0105@gmail.com",
+packages=find_packages(),
+install_requires = [],
+
+)
